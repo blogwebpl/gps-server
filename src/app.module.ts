778359dd4265f85e379383fd54ad8b784a94e0fa
Tomasz Durałek <tomek@blogweb.pl>
2022-12-09 19:07:47 +0100

    add static content

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
 	imports: [
@@ -16,6 +18,9 @@ import { AppController } from './app.controller';
 			useFactory: (config: ConfigService) => {
 				return { uri: config.get<string>('MONGODB_URI') };
 			},
+		}),
+		ServeStaticModule.forRoot({
+			rootPath: join(__dirname, '..', 'client'),
 		}),
 		UsersModule,
 		AuthModule,

b07cae2fe7a1bc86e1a02352395ba04557affea8
Tomasz Durałek <tomek@blogweb.pl>
2022-12-10 19:21:09 +0100

    use global filters

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { MongoExceptionFilter } from './filters/mongo-exception.filter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
@@ -7,6 +8,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.setGlobalPrefix('api');
 	app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+	app.useGlobalFilters(new MongoExceptionFilter());
 	const port = app.get(ConfigService).get('PORT');
 	await app.listen(port, () => {
 		Logger.log(`HTTP(S) server is listening on port ${port}`);
